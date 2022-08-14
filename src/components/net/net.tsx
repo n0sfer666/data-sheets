c3bb06fbba9e983151a1dbcedc8489fdf70bc512
Khasanov Petr <n0sfer666@yandex.ru>
2022-08-14 09:23:38 +0300

    create Net refactor Card, NetElement

---
+++ src/components/net/net.tsx
@@ -0,0 +1,45 @@
+import React, { useState } from 'react';
+import NetElement from '../net-element/net-element';
+import { TNetProps } from './net.types';
+
+const styles = {
+  main: 'Net',
+  header: {
+    main: 'Net__header',
+    text: 'Net__header-text',
+  },
+  content: {
+    main: 'Net__content',
+    item: 'Net__content-item',
+    lines: {
+      horizontal: 'Net__content-line-horizontal',
+      vertical: 'Net__content-line-vertical',
+    },
+  },
+};
+
+function Net({ title, items }: TNetProps) {
+  const [netElementsOpenState, setNetElementsOpenState] = useState(
+    new Array(items.length).fill(false),
+  );
+  return (
+    <div className={styles.main}>
+      <button className={styles.header.main} type="button">
+        <h3 className={styles.header.text}>{title}</h3>
+      </button>
+      <div className={styles.content.main}>
+        <div className={styles.content.lines.vertical} />
+        {
+          items.map((item, index) => (
+            <div key={`Net-item-${index}`} className={styles.content.item}>
+              <div className={styles.content.lines.horizontal} />
+              <NetElement {...item} />
+            </div>
+          ))
+        }
+      </div>
+    </div>
+  );
+}
+
+export default Net;
