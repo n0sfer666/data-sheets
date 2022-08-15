2e829290ea95aee0286e9fd776aef1a5df6c3ed6
Khasanov Petr <n0sfer666@yandex.ru>
2022-08-15 17:47:51 +0300

    refactor Remover style

--- src/components/remover/remover.tsx
+++ src/components/remover/remover.tsx
@@ -6,7 +6,9 @@ import { TRemoverProps } from './remover.types';
 const styles = {
   main: 'Remover',
   button: 'Remover__button',
+  container: 'Remover__container',
   select: 'Remover__select',
+  selectTriangle: 'Remover__select-triangle',
 };
 
 function Remover({
@@ -29,15 +31,22 @@ function Remover({
       </button>
       {
         (isOpen) && (
-        <select
-          className={styles.select}
-          onChange={callbacks.onSelectChange}
-        >
-          <option key={`option-${titles.length}`} value={titles.length}>choose net...</option>
-          {
+        <div className={styles.container}>
+          <select
+            className={styles.select}
+            onChange={callbacks.onSelectChange}
+          >
+            <option key={`option-${titles.length}`} value={titles.length}>choose net...</option>
+            {
             titles.map((title, index) => (<option key={`option-${index}`} value={index}>{title}</option>))
           }
-        </select>
+          </select>
+          <div className={styles.selectTriangle}>
+            <svg width="8" height="4" viewBox="0 0 8 4" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M0.666687 0.666656L4.00002 3.99999L7.33335 0.666656H0.666687Z" fill="#C4C4C4" />
+            </svg>
+          </div>
+        </div>
         )
       }
     </div>
