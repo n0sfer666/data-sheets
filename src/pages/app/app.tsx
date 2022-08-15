a9b7e137c665fbd5f2f8695bb7d3f0c6cb76a64d
Khasanov Petr <n0sfer666@yandex.ru>
2022-08-15 11:57:20 +0300

    fixed app

--- src/pages/app/app.tsx
+++ src/pages/app/app.tsx
@@ -1,9 +1,8 @@
 import React, { useEffect, useRef, useState } from 'react';
-import { container } from 'webpack';
 import Finder from '../../components/finder/finder';
 import Layout from '../../components/layout/layout';
 import Net from '../../components/net/net';
-import Remover from '../../components/remover/remover';
+// import Remover from '../../components/remover/remover';
 import { TAppProps, TChangeEvent } from './app.types';
 
 const styles = {
@@ -61,12 +60,12 @@ function App({ data }: TAppProps) {
   return (
     <Layout footer={(
       <>
-        <Remover
+        {/* <Remover
           elementIndex={footerIndexes.remover}
           onClick={callbacks.onClickFooterElement}
           onChange={callbacks.onChangeRemover}
           isOpen={footerOpenState[footerIndexes.remover]}
-        />
+        /> */}
         <Finder
           elementIndex={footerIndexes.finder}
           onClick={callbacks.onClickFooterElement}
